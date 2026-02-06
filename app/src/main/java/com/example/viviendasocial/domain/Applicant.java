3fb488690bd8568d3bed69d2e8dd90672d69aef4
hermes <a28704@svalero.com>
2026-02-06 13:56:15 +0100

    Añadida clase Applicant

--- app/src/main/java/com/example/viviendasocial/domain/Applicant.java
+++ app/src/main/java/com/example/viviendasocial/domain/Applicant.java
@@ -1,4 +1,21 @@
 package com.example.viviendasocial.domain;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Applicant {
+    private long id;
+    private String name;
+    private String surname;
+    private String dni;
+    private LocalDate birthDate;
+    private int salary;
+    private int familyMembers;
+    private boolean employed;
+    private List<Dwelling> dwellings;
 }
